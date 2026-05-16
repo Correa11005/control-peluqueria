be35b5cd1298725f1ae932882be36c26fb33bb08
Jacob Correa <jacobocorrea777@gmail.com>
2026-05-16 11:17:33 -0500

    Corregir conexion Aiven manteniendo zona horaria Madrid

--- app.py
+++ app.py
@@ -43,11 +43,8 @@ def get_db_connection():
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE"),
         port=int(os.environ.get("MYSQLPORT", 3306)),
+        ssl_disabled=False
     )
-
-    cursor = conn.cursor()
-    cursor.execute(f"SET time_zone = '{TZ_APP}'")
-    cursor.close()
 
     return conn
 
